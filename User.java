ac0ce50ce19f65befbfff3c74b50b7dd9478ac86
Dean Orion <60265116+Doer64@users.noreply.github.com>
2025-01-05 18:05:19 +0200

    Added toLowerCase back

--- User.java
+++ User.java
@@ -44,7 +44,7 @@
     /** If this user follows the given name, returns true; otherwise returns false. */
     public boolean follows(String name) {
         for(int i = 0; i < fCount; i++){
-            if(follows[i].equals(name)) return true;
+            if(follows[i].toLowerCase().equals(name.toLowerCase())) return true;
         }
         return false;
     }
@@ -52,7 +52,7 @@
      *  If this user already follows the given name, or if the follows list is full, does nothing and returns false; */
     public boolean addFollowee(String name) {
         if(fCount>=maxfCount || follows(name) == true){
-            System.out.printf("%s could not follow %s \n", this.name, name);
+            System.out.printf("%s cannot follow %s \n", this.name, name);
             return false;
         }
         follows[fCount] = name;
@@ -65,7 +65,7 @@
      *  If the name is not in the list, does nothing and returns false. */
     public boolean removeFollowee(String name) {
         int index = 0;
-        while (index < fCount && !follows[index].equals(name)) {
+        while (index < fCount && !follows[index].toLowerCase().equals(name.toLowerCase())) {
             index++;
         }
         if(index == fCount){
